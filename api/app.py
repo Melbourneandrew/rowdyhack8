c4e6992192282c7861080db6f995774602a41c57
Andrew Melbourne <melbourneandrew@gmail.com>
2023-03-25 21:48:14 -0500

    Added requirements.txt

--- api/app.py
+++ api/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
 import FirestoreDB
 from controllers.ChatbotController import ask_chatbot
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 FirestoreDB.connect()
 
 @app.route('/')
