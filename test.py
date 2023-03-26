2dbddbb1fe8c940cf5b2d7992392f5b8512d1e84
Jason Antwi-Appah <hey@jasonaa.me>
2023-03-26 05:08:12 -0500

    audio transcription from cli args

--- test.py
+++ test.py
@@ -1,12 +1,16 @@
+import sys
+
 from api.util.index import *
 
-file = "/users/json/desktop/Interview Recordings/short.flac"
-id = unique_id()
-fname = f"{id}.flac"
-blob = file_to_gcs(open(file, 'rb'), fname, "audio/flac")
 
-gcs = f"gs://{AUDIO_BUCKET_NAME}/{fname}"
-print("lol",gcs)
+for file in sys.argv[1]:
+    print(file)
+    id = unique_id()
+    fname = f"{id}.flac"
+    blob = file_to_gcs(open(file, 'rb'), fname, "audio/mp3")
 
-t = transcribe_gcs(gcs, speech_v1.RecognitionConfig.AudioEncoding.FLAC, id)
-print(t)
+    gcs = f"gs://{AUDIO_BUCKET_NAME}/{fname}"
+    print("lol",gcs)
+
+    t = transcribe_gcs(gcs, speech_v1.RecognitionConfig.AudioEncoding.MP3, id)
+    print(t, t.metadata, t)
