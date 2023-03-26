79cbc7f5e381c5c4ea936858cca066bfdff3130a
Jason Antwi-Appah <hey@jasonaa.me>
2023-03-25 22:12:02 -0500

    style changes

--- client/src/pages/chat.js
+++ client/src/pages/chat.js
@@ -1,8 +1,7 @@
 import styles from '@/styles/Chat.module.css'
 
 import React, { useState } from 'react'
-
-
+import Head from "next/head"
 
 export default function Chat(){
 
@@ -10,6 +9,14 @@ export default function Chat(){
         {
             "content": "You are a college professor teaching Marketing 101. When a student brings you a question, you will be provided with a excerpt from your lecture and a textbook passage related to the question to help you answer their query with context.",
             "role": "system"
+        },
+        {
+            "content": "Student question: How do I pass this classLecture excerpt: This is a lecture excerpttextbook passage: This is a textbook passage",
+            "role": "user"
+        },
+        {
+            "content": "To pass this class, you need to be committed, punctual, and hardworking. Attendance is a must, as you cannot contribute or learn if you are not present in the class. Active participation in class discussions and group activities can improve your chances of passing. You must complete all the assignments and study regularly for continuous learning. Additionally, seek feedback from your peers and the professor to identify where you need to improve.\n\nAs per the textbook, the key to passing any class includes setting achievable goals, developing a study plan, managing time effectively, staying organized, practicing self-discipline, and seeking help when needed. Setting a goal for what you want to achieve in class can help you focus on studying efficiently. Developing a study plan and managing time effectively will help you stay on track and avoid procrastination. Staying organized and using tools like a planner or calendar can help you balance your class work, projects, and assignments. Discipline is vital as you are responsible for your education and need to make the right choices. Lastly, seek help from peers, professors, and tutors when needed. They can provide you with additional guidance or support to help you succeed.",
+            "role": "assistant"
         },
         {
             "content": "Student question: How do I pass this classLecture excerpt: This is a lecture excerpttextbook passage: This is a textbook passage",
@@ -38,9 +45,15 @@ export default function Chat(){
                 <>{
                     // TODO: don't use randomUUID as key
                     messages.filter((msg)=>msg.role.toLowerCase() !== "system").map(({content, role}) => 
-                    <div className={`${styles.message} ${role === "assistant" ? styles.assistant_msg : styles.user_msg}`} key={crypto.randomUUID()}>
-
-                        <p>{role}: {content}</p>
+                    <div className={`${styles.message} ${role === "assistant" ? styles.assistant_msg : styles.user_msg}`}>
+                        <div className={styles.icon}>
+                        {role == "user" ? <span class="material-symbols-outlined">
+account_circle
+</span> : <span class="material-symbols-outlined">
+neurology
+</span>}
+                        </div>
+                        <p>{content}</p>
                     </div>
                     )
                 }</>
@@ -59,6 +72,9 @@ export default function Chat(){
 
     return(
         <div className={styles.container}>
+            <Head>
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+            </Head>
             <div className={styles.title}>
                 <p>Knowlater</p>
             </div>
